88a8b2cc394809fc033fc68facbe04b4520d9c33
alex-gre <alexstv@inbox.ru>
2023-10-27 21:04:27 +0300

    add catalog irbis_server for os ubuntu16_04

---
+++ irbis_server/src/php/phone_rec_pdo/create_mytable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `mynotepad` (
+  `id` int(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf32 NOT NULL,
+  `phone` varchar(21) , 
+  `text` text NOT NULL
+)
---
+++ irbis_server/src/php/phone_rec_pdo/insert_mynotepad.php
@@ -0,0 +1,39 @@
+﻿<?php
+
+if (isset($_POST['name']) && isset($_POST['phone']) && isset($_POST['text'])){
+    // Переменные с формы
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $text = $_POST['text'];
+    
+    // Параметры для подключения
+    $db_host = "localhost"; 
+    $db_user = "root"; // Логин БД
+    $db_password = "qwerty"; // Пароль БД
+    $db_base = 'mydb'; // Имя БД
+    $db_table = "mynotepad"; // Имя Таблицы БД
+    
+    try {
+        // Подключение к базе данных
+        $db = new PDO("mysql:host=$db_host;dbname=$db_base", $db_user, $db_password);
+        // Устанавливаем корректную кодировку
+        $db->exec("set names utf8");// latin only
+        // Собираем данные для запроса
+        $data = array( 'name' => $name,'phone' => $phone, 'text' => $text ); 
+        // Подготавливаем SQL-запрос
+        $query = $db->prepare("INSERT INTO $db_table (name,phone,text) values (:name,:phone,:text)");
+        // Выполняем запрос с данными
+        $query->execute($data);
+        // Запишим в переменую, что запрос отрабтал
+        $result = true;
+    } catch (PDOException $e) {
+        // Если есть ошибка соединения или выполнения запроса, выводим её
+        print "Ошибка!: " . $e->getMessage() . "<br/>";
+    }
+    
+    if ($result) {
+    	echo "Успех. Информация занесена в базу данных";
+    }
+}
+
+?>
